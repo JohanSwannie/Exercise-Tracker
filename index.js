aa088022a72bec477110abbaf73459897b03fc0f
Johan Swan <jswannieman@gmail.com>
2024-03-22 08:43:04 +1300

    Update index JS Component

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const bodyParser = require("body-parser");
 const { mongoose, Schema, model } = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
